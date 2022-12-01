4df7d94ecface56ddd01fd8b61412d5c0f269f1f
Ilham Faizal Hamka <ilham.faizal.hamka@mail.ugm.ac.id>
2022-12-01 12:10:35 +0700

    use cleaned datasets

--- WC-prediction.py
+++ WC-prediction.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[30]:
+# In[77]:
 
 
 import pandas as pd
@@ -9,13 +9,13 @@ import pickle
 from scipy.stats import poisson
 
 
-# In[31]:
+# In[78]:
 
 
 df_WC_2022 = pd.read_csv('WC_Datasets/wc_matches_2022_crawl.csv')
 
 
-# In[32]:
+# In[79]:
 
 
 dict_table = pickle.load(open('WC_Datasets/dict_table','rb'))
@@ -23,7 +23,7 @@ df_historical_matches = pd.read_csv('WC_Datasets/clean_fifa_worldcup_matches.csv
 df_fixture = pd.read_csv('WC_Datasets/clean_fifa_worldcup_fixture.csv')
 
 
-# In[33]:
+# In[80]:
 
 
 # df_historical_data = df_historical_matches.append(df_WC_2022, ignore_index=True)
@@ -33,7 +33,7 @@ df_historical_data = pd.concat([df_historical_matches,df_WC_2022])
 
 # ## Calculate Team Strength
 
-# In[34]:
+# In[81]:
 
 
 df_home = df_historical_data[['HomeTeam', 'HomeGoals', 'AwayGoals']]
@@ -48,7 +48,7 @@ df_team_strength
 
 # ## Function Predict_points
 
-# In[35]:
+# In[82]:
 
 
 def predict_points(home, away):
@@ -76,7 +76,7 @@ def predict_points(home, away):
 
 # ## Testing Function
 
-# In[36]:
+# In[83]:
 
 
 print(predict_points('England', 'Wales'))
@@ -84,7 +84,7 @@ print(predict_points('Argentina', 'Saudi Arabia'))
 print(predict_points('Qatar (H)', 'Ecuador')) # Qatar vs Team X -> 0 points to both
 
 
-# In[37]:
+# In[84]:
 
 
 df_fixture_group_48 = df_fixture[:48].copy()
@@ -94,7 +94,7 @@ df_fixture_semi = df_fixture[60:62].copy()
 df_fixture_final = df_fixture[62:].copy()
 
 
-# In[38]:
+# In[85]:
 
 
 for group in dict_table:
@@ -111,49 +111,49 @@ for group in dict_table:
     dict_table[group] = dict_table[group].round(0)
 
 
-# In[39]:
+# In[86]:
 
 
 dict_table['Group A']
 
 
-# In[40]:
+# In[87]:
 
 
 dict_table['Group B']
 
 
-# In[41]:
+# In[88]:
 
 
 dict_table['Group C']
 
 
-# In[42]:
+# In[89]:
 
 
 dict_table['Group D']
 
 
-# In[43]:
+# In[90]:
 
 
 dict_table['Group E']
 
 
-# In[44]:
+# In[91]:
 
 
 dict_table['Group F']
 
 
-# In[45]:
+# In[92]:
 
 
 dict_table['Group G']
 
 
-# In[46]:
+# In[93]:
 
 
 dict_table['Group H']
@@ -161,13 +161,13 @@ dict_table['Group H']
 
 # ## Knock Out
 
-# In[47]:
+# In[94]:
 
 
 df_fixture_knockout
 
 
-# In[48]:
+# In[95]:
 
 
 for group in dict_table:
@@ -180,7 +180,7 @@ df_fixture_knockout['winner'] = '?'
 df_fixture_knockout
 
 
-# In[49]:
+# In[96]:
 
 
 def get_winner(df_fixture_updated):
@@ -195,7 +195,7 @@ def get_winner(df_fixture_updated):
     return df_fixture_updated
 
 
-# In[50]:
+# In[97]:
 
 
 get_winner(df_fixture_knockout)
@@ -203,7 +203,7 @@ get_winner(df_fixture_knockout)
 
 # ## Quarter Final
 
-# In[51]:
+# In[98]:
 
 
 def update_table(df_fixture_round_1, df_fixture_round_2):
@@ -215,14 +215,14 @@ def update_table(df_fixture_round_1, df_fixture_round_2):
     return df_fixture_round_2
 
 
-# In[52]:
+# In[99]:
 
 
 
 update_table(df_fixture_knockout, df_fixture_quarter)
 
 
-# In[53]:
+# In[100]:
 
 
 get_winner(df_fixture_quarter)
@@ -230,13 +230,13 @@ get_winner(df_fixture_quarter)
 
 # ## SemiFinal
 
-# In[54]:
+# In[101]:
 
 
 update_table(df_fixture_quarter, df_fixture_semi)
 
 
-# In[55]:
+# In[102]:
 
 
 get_winner(df_fixture_semi)
@@ -244,26 +244,14 @@ get_winner(df_fixture_semi)
 
 # ## Final
 
-# In[56]:
+# In[103]:
 
 
 update_table(df_fixture_semi, df_fixture_final)
 
 
-# In[57]:
+# In[104]:
 
 
 get_winner(df_fixture_final)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
